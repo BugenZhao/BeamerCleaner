3a7bbe8d25d5102f3e77f65152b15165a7604b6c
BugenZhao <bugenzhao@qq.com>
2020-03-18 11:50:13 +0800

    Initial commit

---
+++ beamerc.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import sys
+import os
+from tqdm import tqdm
+
+infile = None
+
+try:
+    infile = PdfFileReader(sys.argv[1], 'rb')
+except IndexError:
+    sys.stderr.write(f'Usage: {sys.argv[0]} [ file ]\n')
+    sys.exit(-1)
+except:
+    sys.stderr.write(f'Cannot read input file: {sys.argv[1]}\n')
+    sys.exit(-2)
+
+infos = infile.documentInfo.copy()
+
+if len(list(filter(lambda s: 'beamer' in str(s).lower(), infos.values()))) == 0:
+    sys.stderr.write('Not a Beamer PDF\n')
+    sys.exit(-3)
+
+output = PdfFileWriter()
+page_nums = []
+output_page_nums = []
+
+print('Analyzing...')
+for page in tqdm(infile.pages):
+    text = str(page.extractText())
+    page_nums.append(text.splitlines()[-1])
+
+print('Comparing...')
+for i in tqdm(range(1, infile.getNumPages())):
+    if page_nums[i] != page_nums[i - 1]:
+        output_page_nums.append(i - 1)
+output_page_nums.append(-1)
+
+print('Generating...')
+for num in tqdm(output_page_nums):
+    output.addPage(infile.pages[num])
+
+infos['/Producer'] += ' + BeamerCleaner'
+output.addMetadata(infos)
+
+dirname = os.path.dirname(sys.argv[1])
+basename = os.path.basename(sys.argv[1]).replace('.pdf', '_C.pdf')
+output_name = os.path.join(dirname, basename)
+
+print(f'Writing to {output_name}...')
+with open(output_name, 'wb') as f:
+    output.write(f)
+
+print('Done.')
